6d057d70b4b774f37b1824d52708ccf0a69ef21c
alruswall <114301518+alruswall@users.noreply.github.com>
2023-04-12 19:56:01 -0500

    Add files via upload
    I'm thinking this might be correct since running fsm.do returns no errors in it, only my TB.

--- Project/SV/gol.sv
+++ Project/SV/gol.sv
@@ -1,11 +1,18 @@
-module gol(start, clk, outGrid)
- input start;
- output [63:0] outGrid;
- assign grid = 64'h4020_E000_0000_0000;
+module gol(start, clk, outGrid);
+ input logic start;
+ input logic clk;
+ output logic [63:0] outGrid;
+logic [63:0] grid;
+logic [63:0] currentGrid;
+logic [63:0] nextGrid;
  datapath prog(currentGrid, nextGrid);
+ assign grid = 64'h4020_E000_0000_0000;
+
+
 
    typedef enum 	logic [3:0] {S0, cal, st} statetype;
-   always_ff @(posedge clk, posedge reset)
+   statetype state, nextstate;
+   always_ff @(posedge clk, posedge start)
      if (start) state <= S0;
      else       state <= nextstate;
 
@@ -13,7 +20,7 @@ module gol(start, clk, outGrid)
      case (state)
 //initial state
       S0: begin
-      outGrid = grid;
+        assign outGrid = grid;
       nextstate = cal;
       end
 //put into datapath
@@ -23,37 +30,8 @@ module gol(start, clk, outGrid)
       end
       st: begin
 //set to grid
-        outGrid = nextGrid;
+       assign outGrid = nextGrid;
         nextstate = cal;
       end
      endcase
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 endmodule
